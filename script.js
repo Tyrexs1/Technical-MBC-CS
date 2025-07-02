fb8e7435615779c2ea317b8b2fdf3f7d4df91a5c
Tyrexs1 <robyariga336@gmail.com>
2025-07-02 21:47:59 +0700

    Final configuration for Vercel

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     function loadContent() {
-        fetch('/api/api.php')
+        fetch('api.php')
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`Network response was not ok: ${response.statusText}`);
