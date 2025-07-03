aea7c84a390f6eca507f240b99cfde258f99d7f5
Tyrexs1 <robyariga336@gmail.com>
2025-07-03 14:20:40 +0700

    change name index.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     function loadContent() {
-        fetch('/api')
+        fetch('/index')
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`Network response was not ok: ${response.statusText}`);
