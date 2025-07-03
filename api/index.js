8f2945b941abf2ecf29d5153635e6a9e86590e57
Tyrexs1 <robyariga336@gmail.com>
2025-07-03 14:43:33 +0700

    final

--- api/index.js
+++ api/index.js
@@ -1,24 +1,24 @@
 const siteData = {
-  about_text: "MBC Laboratory, singkatan dari Multimedia, Big Data, dan Cyber Security Laboratory, merupakan ...",
+  about_text: "MBC Laboratory, singkatan dari Multimedia, Big Data, dan Cyber Security Laboratory, merupakan salah satu entitas penelitian yang beroperasi di bawah Kementerian Komunikasi dan Multimedia (KK NCM). Fokus utama laboratorium ini adalah mempelajari dan mengembangkan pengetahuan di bidang Cyber Security, Big Data, dan Multimedia. Didirikan pada tanggal 4 Oktober 2019, MBC Laboratory telah menjadi pusat penelitian yang berdedikasi untuk memahami, mengatasi, dan mengembangkan solusi terkini dalam tiga bidang utamanya.",
   divisions: [
     {
       title: "Cyber Security",
-      description: "Menganalisis dan mengembangkan teknik pertahanan untuk ...",
+      description: "Cyber security atau keamanan siber adalah tindakan perlindungan perangkat, jaringan, program, dan data dari ancaman serangan siber serta akses ilegal.",
       image_url: "/Asset/cyber.jpg"
     },
     {
       title: "Game Tech",
-      description: "Merancang dan mengembangkan teknologi interaktif ...",
+      description: "Game tech atau teknologi game adalah teknologi yang digunakan untuk mengembangkan dan memproduksi game.",
       image_url: "/Asset/gt.jpg"
     },
     {
       title: "Big Data",
-      description: "Mengolah dan menganalisis set data bervolume besar ...",
+      description: "Big data adalah kumpulan data yang sangat besar dan kompleks yang terdiri dari data terstruktur, semi-terstruktur, dan tidak terstruktur. Big data memiliki beberapa karakteristik, yaitu volume, kecepatan, dan variasi.",
       image_url: "/Asset/data.jpg"
     },
     {
       title: "Geographic Information System",
-      description: "Mengintegrasikan data spasial untuk visualisasi ...",
+      description: "Sistem Informasi Geografis (Geographic Information System atau GIS) adalah sebuah teknologi yang digunakan untuk mengumpulkan, mengelola, menganalisis, dan memvisualisasikan data berbasis lokasi atau geografis.",
       image_url: "/Asset/gis.jpg"
     }
   ],
